4efeacadfd084e32bc2873445cc5f94a37a45284
Vu Minh Tien <minhtienvu2210@gmail.com>
2023-03-13 14:15:01 +0700

    Rename variable

--- app/services/google_searcher_services.rb
+++ app/services/google_searcher_services.rb
@@ -14,15 +14,15 @@ module GoogleSearcherServices
       file_upload = current_user.file_uploads.create(file_name: file.original_filename)
 
       data.each_with_index do |row, index|
-        next if index < Constants::START_READ_FROM || row.blank?
+        next if row.blank? || index == Constants::HEADER_ROW
 
         keyword = row.first
-        response = HTTParty.get(google_search_query(keyword))
+        google_search_response = HTTParty.get(google_search_query(keyword))
 
-        return raise StandardError, response['error']['message'] if response.code != 200
+        return raise StandardError, google_search_response['error']['message'] if google_search_response.code != 200
 
         google_page_html = crawl_google_page(keyword)
-        statistics << generate_response(keyword, google_page_html, response['searchInformation'])
+        statistics << generate_response(keyword, google_page_html, google_search_response['searchInformation'])
       end
       file_upload.statistics.insert_all statistics
 
